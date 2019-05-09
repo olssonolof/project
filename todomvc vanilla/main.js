58ba7d2943ec87ca1960f40e074a4fcac1026827
Olof <olssonolof@hotmail.com>
2019-05-09 11:15:45 +0200

    function to create html with js

--- todomvc vanilla/main.js
+++ todomvc vanilla/main.js
@@ -1,9 +1,26 @@
 const toggleAll = document.querySelector("#toggleall");
+const list = document.querySelector("#todoList");
+
 toggleAll.addEventListener("keypress", function(e) {
   let key = e.charCode;
   if (key === 13) {
+    let test = toggleAll.value;
     createListItem();
   }
 });
 
-const createListItem = () => {};
+const createListItem = () => {
+  let node = document.createElement("li");
+  let div = document.createElement("div");
+  node.appendChild(div);
+  let input = document.createElement("input");
+  div.appendChild(input);
+  let label = document.createElement("label");
+  div.appendChild(label);
+  label.textContent = toggleAll.value;
+  let button = document.createElement("button");
+  div.appendChild(button);
+  list.appendChild(node);
+  console.log("let node" + node);
+  console.log("let todolist" + list);
+};
