0cf669031e359e2a8ad15eab40d5612cd81fc1dd
Olof <olssonolof@hotmail.com>
2019-05-09 15:09:35 +0200

    js func to todolist done

--- todomvc-vanilla/main.js
+++ todomvc-vanilla/main.js
@@ -16,20 +16,27 @@ const createListItem = () => {
   let div = document.createElement("div");
   node.appendChild(div);
   let input = document.createElement("input");
+  input.type = "checkbox";
   div.appendChild(input);
   let label = document.createElement("label");
   div.appendChild(label);
-  input.hidden = true;
   label.innerText = toggleAll.value;
-  input.value = toggleAll.value;
+  label.classList = "listitem";
   toggleAll.value = "";
   let button = document.createElement("button");
+  input.addEventListener("click", () => {
+    isChecked(input.checked, label);
+  });
   div.appendChild(button);
   list.appendChild(node);
-  node.addEventListener("dblclick", () => {
-    label.hidden = true;
-    input.hidden = false;
-  });
+};
+
+const isChecked = (value, label) => {
+  if (value === true) {
+    label.classList = "listitemchecked";
+  } else {
+    label.classList.remove("listitemchecked");
+  }
 };
 
 const showMarkAll = () => {
